808bb50f229802b7a01647429ba9ad76cbbc29f7
kingdapster <sal.carrazco89@gmail.com>
2023-11-08 12:24:15 -0800

    created html skeleton, linked js page, and added api url to js page, not tested

---
+++ index.js
@@ -0,0 +1,11 @@
+state ={
+    allArtist: []
+}
+ 
+const getAllArtists = async() =>{
+    const response = await fetch (`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf/artists`)
+console.log(getAllArtists)
+
+}
+
+getAllArtists()
